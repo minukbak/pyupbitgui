c5d22572307e6cf50b4d173836610abd6ac6ad33
minukbak <minukbak@gmail.com>
2021-10-12 22:55:00 +0900

    이평선 조회 함수 변경

--- strategies/tts3ma.py
+++ strategies/tts3ma.py
@@ -5,13 +5,13 @@ import time
 import json
 import pprint
 
-# Calculate The Moving Average
+# Calculate The Moving Average (이동 평균선 계산)
 def calMvAvg(ticker, timIntv, cnt):
   df = pyupbit.get_ohlcv(ticker, timIntv, cnt+1)
   mvAvg = df['close'].rolling(cnt).mean().iloc[-2]
   return mvAvg
 
-# Condition of Buying
+# Condition of Buying (매수 조건)
 def condxBuy(ticker, timIntv, mvAvg1, mvAvg2):
   calMvAvg1 = calMvAvg(ticker, timIntv, mvAvg1)
   calMvAvg2 = calMvAvg(ticker, timIntv, mvAvg2)
@@ -19,7 +19,7 @@ def condxBuy(ticker, timIntv, mvAvg1, mvAvg2):
     return True
   return False
 
-# Condition of Selling
+# Condition of Selling (매도 조건)
 def condxSell(ticker, timIntv, mvAvg1, mvAvg2):
   calMvAvg1 = calMvAvg(ticker, timIntv, mvAvg1)
   calMvAvg2 = calMvAvg(ticker, timIntv, mvAvg2)
@@ -33,38 +33,38 @@ with open('config.json', 'r') as conf:
 access = config['DEFAULT']['ACCESS_KEY'] 
 secret = config['DEFAULT']['SECRET_KEY']
 
-upbit = pyupbit.Upbit(access, secret) # class instance, object
+upbit = pyupbit.Upbit(access, secret)
 
-def main(ticker, interval, MA1, MA2, amount):
+def main(ticker, timIntv, mvAvg1, mvAvg2, amount):
   
-  hold = False  # 현재 코인 보유 여부
-  op_mode = False # 시작 동시 매수 방지
+  holding = False  # 현재 코인 보유 여부
+  operMode = False # 시작 동시 매수 방지
 
   while True:
     now = datetime.datetime.now().strftime("%H:%M:%S")
 
     # 시작 동시 매수 방지: Sell condition은 MA1 <= MA2 이고 이 상태에서는 매수되지 않음
-    if condxSell(ticker, interval, MA1, MA2) is True:
-      op_mode = True
+    if condxSell(ticker, timIntv, mvAvg1, mvAvg2) is True:
+      operMode = True
 
-    # 매도 시도
-    # 매도 조건이 True이고, 해당 코인을 가지고 있을 때
-    if hold is True and op_mode is True:
-      if condxSell(ticker, interval, MA1, MA2) is True:
-        ticker_balance = upbit.get_balance(ticker)
-        resp = upbit.sell_market_order(ticker, ticker_balance)
-        hold = False
+    # 매도
+    # 해당 코인을 가지고 있고, 매도 조건이 True일 때
+    if holding is True and operMode is True:
+      if condxSell(ticker, timIntv, mvAvg1, mvAvg2) is True:
+        balTicker = upbit.get_balance(ticker)
+        resp = upbit.sell_market_order(ticker, balTicker)
+        holding = False
         pprint.pprint(resp)
 
-    # 매수 시도
-    # 매수 조건이 True이고, 해당 코인을 가지고 있지 않을 때
-    if hold is False and op_mode is True:
-      if condxBuy(ticker, interval, MA1, MA2) is True:
+    # 매수
+    # 해당 코인을 가지고 있지 않고, 매수 조건이 True일 때
+    if holding is False and operMode is True:
+      if condxBuy(ticker, timIntv, mvAvg1, mvAvg2) is True:
         resp = upbit.buy_market_order(ticker, amount)
-        hold = True
+        holding = True
         pprint.pprint(resp)
 
     # 상태 출력
-    print(f"현재시간 {now} / 적용코인: {ticker} / 적용MA: ({MA1}, {MA2}) / 보유상태: {hold}")
+    print(f"현재시간 {now} / 적용코인: {ticker} / 적용MA: ({mvAvg1}, {mvAvg2}) / 보유상태: {holding}")
 
     time.sleep(1)
