05bbc46cce9e7c6c3c119bfa4eb11bac750a86d8
minukbak <minukbak@gmail.com>
2021-10-12 22:37:10 +0900

    변수명 수정

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from strategies import tts3ma
 
 ticker = "KRW-STPT" # 프로그램 적용 코인
-interval = "minute1" # 봉 단위, minute1 = 1분봉
-MA1 = 7 # 첫 번째 이동평균선 적용 값
-MA2 = 30 # 두 번째 이동평균선 적용 값
+timIntv = "minute1" # 봉 단위, minute1 = 1분봉
+mvAvg1 = 7 # 첫 번째 이동평균선 적용 값
+mvAvg2 = 30 # 두 번째 이동평균선 적용 값
 amount = 10000 # 프로그램 시작 금액
 
-tts3ma.main(ticker,interval, MA1, MA2, amount)
+tts3ma.main(ticker, timIntv, mvAvg1, mvAvg2, amount)
