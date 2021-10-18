78689f0e560b1d5afc3462a9ecac6e373e3b574b
minukbak <minukbak@gmail.com>
2021-10-18 23:06:44 +0900

    프레임 생성

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from tkinter import *
+
 from strategies import tts3ma
 
 ticker = "KRW-STPT" # 프로그램 적용 코인
@@ -6,4 +8,12 @@ mvAvg1 = 7 # 첫 번째 이동평균선 적용 값
 mvAvg2 = 30 # 두 번째 이동평균선 적용 값
 amount = 10000 # 프로그램 시작 금액
 
-tts3ma.main(ticker, timIntv, mvAvg1, mvAvg2, amount)
+# tts3ma.main(ticker, timIntv, mvAvg1, mvAvg2, amount)
+
+root = Tk()
+root.title("Nado GUI")
+root.geometry("640x480+100+100") # 가로 * 세로 + x좌표 + y좌표
+
+root.resizable(False, False) # x(너비), y(높이) 값 변경 불가 (창 크기 변경 불가)
+
+root.mainloop()
