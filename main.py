a7bd59bdf54fe8a69d50df84634e8b58e69b4729
minukbak <minukbak@gmail.com>
2021-10-19 22:38:25 +0900

    로그 기록

--- main.py
+++ main.py
@@ -15,11 +15,24 @@ root = Tk()
 root.title("UpbitAuto")
 root.geometry("640x480") # 가로 * 세로 (+ x좌표 + y좌표)
 
+# 로그 기록 파일
+filename = "log.txt"
+
+def open_file():
+  if os.path.isfile(filename): # 파일 있으면 True, 없으면 False
+    with open(filename, "r", encoding="utf8") as file:
+      txt.delete("1.0", END) # 텍스트 위젯 본문 삭제
+      txt.insert(END, file.read()) # 파일 내용을 본문에 입력
+
+def save_file():
+  with open(filename, "w", encoding="utf8") as file:
+    file.write(txt.get("1.0", END)) # 모든 내용을 가져와서 저장
+
 menu = Menu(root)
 
 menu_file = Menu(menu, tearoff=0)
-menu_file.add_command(label="열기")
-menu_file.add_command(label="저장")
+menu_file.add_command(label="열기", command=open_file)
+menu_file.add_command(label="저장", command=save_file)
 menu_file.add_separator()
 menu_file.add_command(label="끝내기", command=root.quit)
 menu.add_cascade(label="파일", menu=menu_file)
@@ -29,6 +42,10 @@ menu.add_cascade(label="편집")
 menu.add_cascade(label="서식")
 menu.add_cascade(label="보기")
 menu.add_cascade(label="도움말")
+
+# 본문 영역
+txt = Text(root)
+txt.pack(side="left", fill="both", expand=True)
 
 root.config(menu=menu)
 
