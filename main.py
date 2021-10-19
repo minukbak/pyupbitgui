2af712aa164db0a6ed1e26f888fc00c858f85dac
minukbak <minukbak@gmail.com>
2021-10-19 22:32:45 +0900

    GUI 메뉴추가

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from tkinter import *
 
 from strategies import tts3ma
@@ -11,9 +12,24 @@ amount = 10000 # 프로그램 시작 금액
 # tts3ma.main(ticker, timIntv, mvAvg1, mvAvg2, amount)
 
 root = Tk()
-root.title("Nado GUI")
-root.geometry("640x480+100+100") # 가로 * 세로 + x좌표 + y좌표
+root.title("UpbitAuto")
+root.geometry("640x480") # 가로 * 세로 (+ x좌표 + y좌표)
 
-root.resizable(False, False) # x(너비), y(높이) 값 변경 불가 (창 크기 변경 불가)
+menu = Menu(root)
+
+menu_file = Menu(menu, tearoff=0)
+menu_file.add_command(label="열기")
+menu_file.add_command(label="저장")
+menu_file.add_separator()
+menu_file.add_command(label="끝내기", command=root.quit)
+menu.add_cascade(label="파일", menu=menu_file)
+
+# 편집, 서식, 보기, 도움말
+menu.add_cascade(label="편집")
+menu.add_cascade(label="서식")
+menu.add_cascade(label="보기")
+menu.add_cascade(label="도움말")
+
+root.config(menu=menu)
 
 root.mainloop()
