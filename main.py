f1f522f564befacfda1e79ef640e38332bef8e68
minukbak <minukbak@gmail.com>
2021-11-13 17:39:02 +0900

    Frame 1차 마무리

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ amount = 10000 # 프로그램 시작 금액
 
 root = Tk()
 root.title("UpbitAuto")
-root.geometry("640x480+100+100") # 가로 * 세로 + x좌표 + y좌표
+root.geometry("640x500+100+100") # 가로 * 세로 + x좌표 + y좌표
 
 # 로그 기록 파일
 filename = "log.txt"
@@ -48,66 +48,69 @@ menu_help.add_command(label="About")
 menu.add_cascade(label="Help", menu=menu_help)
 
 # Option Frame
-frame_option = Frame(root)
-frame_option.pack(padx=5, pady=5, ipady=5)
+frame_option = LabelFrame(root, text=" Options ")
+frame_option.pack(fill="x", padx=5, pady=5)
 
 # 1. Ticker Option
 # Ticker Label
-lbl_ticker = Label(frame_option, text="Ticker", width=8)
+lbl_ticker = Label(frame_option, text="Ticker", width=6)
 lbl_ticker.pack(side="left", padx=5, pady=5)
 # Ticker Combobox
 opt_ticker = ["BTC", "ETH", "DOT"]
-cmb_ticker = ttk.Combobox(frame_option, state="readonly", values=opt_ticker, width=10)
+cmb_ticker = ttk.Combobox(frame_option, state="readonly", values=opt_ticker, width=8)
 cmb_ticker.current(0)
 cmb_ticker.pack(side="left", padx=5, pady=5)
 
 # 2. TimIntv Option
 # TimIntv Label
-lbl_timIntv = Label(frame_option, text="TimIntv", width=8)
+lbl_timIntv = Label(frame_option, text="TimIntv", width=6)
 lbl_timIntv.pack(side="left", padx=5, pady=5)
 # TimIntv Combobox
 opt_timIntv = ["3", "7", "15", "30"]
-cmb_timIntv = ttk.Combobox(frame_option, state="readonly", values=opt_timIntv, width=10)
+cmb_timIntv = ttk.Combobox(frame_option, state="readonly", values=opt_timIntv, width=8)
 cmb_timIntv.current(0)
 cmb_timIntv.pack(side="left", padx=5, pady=5)
 
 # 3. MvAvg Option
 # MvAvg Label
-lbl_mvAvg = Label(frame_option, text="MvAvg", width=8)
+lbl_mvAvg = Label(frame_option, text="MvAvg", width=6)
 lbl_mvAvg.pack(side="left", padx=5, pady=5)
 # MvAvg Combobox
 opt_mvAvg = ["3", "7", "20"]
-cmb_mvAvg = ttk.Combobox(frame_option, state="readonly", values=opt_mvAvg, width=10)
+cmb_mvAvg = ttk.Combobox(frame_option, state="readonly", values=opt_mvAvg, width=8)
 cmb_mvAvg.current(0)
 cmb_mvAvg.pack(side="left", padx=5, pady=5)
 
-# 4. Amount Option & Save Button
-lbl_amount = Label(frame_option, text="Amount", width=8)
+# 4. Amount Option
+# Amount Label
+lbl_amount = Label(frame_option, text="Amount", width=6)
 lbl_amount.pack(side="left", padx=5, pady=5)
 
-btn_save = Button(frame_option, padx=5, pady=5, width=12, text="Save")
-btn_save.pack(side="right")
+# Amount TextBox
+str = StringVar()
+tBox_Amt = ttk.Entry(frame_option, width=20, textvariable=str)
+tBox_Amt.pack(side="left", padx=5, pady=5)
 
 # Log Frame
-frame_log = Frame(root)
+frame_log = LabelFrame(root, text=" Log ")
 frame_log.pack(fill="both", padx=5, pady=5)
 # 스크롤 바
 scrollbar = Scrollbar(frame_log)
 scrollbar.pack(side="right", fill="y")
 
 # 로그 영역
-log_file = Text(frame_log, height=15, yscrollcommand=scrollbar.set)
+log_file = Text(frame_log, height=20, yscrollcommand=scrollbar.set)
 log_file.pack(side="left", fill="both", expand=True)
 scrollbar.config(command=log_file.yview)
 
 # Result Frame
-result_frame = Frame(root)
+result_frame = LabelFrame(root, text=" Result ")
 result_frame.pack(fill="both", padx=5, pady=5)
 
 scrollbar = Scrollbar(result_frame)
 scrollbar.pack(side="right", fill="y")
 
-result_file = Text(result_frame, height=10, yscrollcommand=scrollbar.set)
+result_file = Text(result_frame, height=5, yscrollcommand=scrollbar.set)
 result_file.pack(side="left", fill="both", expand=True)
 scrollbar.config(command=result_file.yview)
 
@@ -115,14 +118,13 @@ scrollbar.config(command=result_file.yview)
 frame_execute = Frame(root)
 frame_execute.pack(fill="x", padx=5, pady=5)
 
-# Start Button
-btn_start = Button(frame_execute, padx=5, pady=5, width=12, text="Start")
-btn_start.pack(side="left")
-
 # End Button
 btn_end = Button(frame_execute, padx=5, pady=5, width=12, text="End")
-btn_end.pack(side="right")
+btn_end.pack(side="right", padx=5)
 
+# Start Button
+btn_start = Button(frame_execute, padx=5, pady=5, width=12, text="Start")
+btn_start.pack(side="right", padx=5)
 
 root.config(menu=menu)
 
